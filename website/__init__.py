67b6bed552cf4f7ac842786ed08b1768245e94a6
DJsega1 <DJsega1@yandex.ru>
2022-04-30 20:58:46 +0300

    Fixed models create

--- website/__init__.py
+++ website/__init__.py
@@ -7,12 +7,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db.sqlite"
 
 db = SQLAlchemy()
 db.init_app(app)
-db.create_all(app=app)
 
 from website.blueprints.main import main as main_blueprint
+
 app.register_blueprint(main_blueprint)
 from website.blueprints.auth import auth as auth_blueprint
+
 app.register_blueprint(auth_blueprint)
 
 import website.views
 import website.models
+
+db.create_all(app=app)
