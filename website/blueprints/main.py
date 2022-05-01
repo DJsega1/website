0f7934fb787cfc185780991673523a0a53a18a60
DJsega1 <DJsega1@yandex.ru>
2022-05-01 15:50:33 +0300

    Main page 70%

--- website/blueprints/main.py
+++ website/blueprints/main.py
@@ -7,13 +7,13 @@ print(__name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    # TODO: Добавить сессию пользователя
+    params = {
+        "logined": False
+    }
+    return render_template('index.html', **params)
 
 
 @main.route('/profile')
 def profile():
     return "Profile!"
-#############
-## https://flask-russian-docs.readthedocs.io/ru/latest/patterns/packages.html
-#############
-
