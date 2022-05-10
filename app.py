b44936554ae5fb37f04d1d3c129913efbfb390b0
DJsega1 <DJsega1@yandex.ru>
2022-05-10 17:05:03 +0300

    refactor

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from website import app
 import os
 
-if __name__ == "__main__":
-    port_nr = os.getenv('PORT', default=8000)
-    app.run(port=port_nr, host='0.0.0.0')
+port_nr = os.getenv('PORT', default=8000)
+app.run(port=port_nr, host='0.0.0.0')
